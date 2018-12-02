28fc883b7e9cc84019bb1feab516f3489957f476
Sjoerd <svanhouten@gmail.com>
2018-12-02 15:05:13 +0100

    day 2

---
+++ src/day02boxes/Checksum.hs
@@ -0,0 +1,21 @@
+module Checksum where
+
+import System.IO
+import Data.Map
+import Data.List.Split
+
+main = do
+        contents <- readFile "boxids.txt"
+        let boxids = splitOn "\n" contents
+        let charCountList = Prelude.map (getCharCountsPerWord) boxids
+        let doubles = countDoubles charCountList
+        let trips = countTriples charCountList
+        print (doubles * trips)
+
+getCharCountsPerWord str = Prelude.map (snd) (toList (fromListWith (+) [(c, 1) | c <- str]))
+
+countDoubles charcountsList = countWordsWithCharOccurrence charcountsList 2
+
+countTriples charcountsList = countWordsWithCharOccurrence charcountsList 3
+
+countWordsWithCharOccurrence charcountsList nr = sum ([ 1 | charcounts <- charcountsList, any ( nr ==) charcounts])
---
+++ src/day02boxes/IdenticalBox.hs
@@ -0,0 +1,32 @@
+module IdenticalBox where
+
+import System.IO
+import Data.Map
+import Data.List.Split
+import Data.List
+import Data.Maybe
+
+main = do
+        contents <- readFile "boxids.txt"
+        let boxids = splitOn "\n" contents
+        let foundWordPair = findTheWord boxids
+        let zippedWords = zip (fst foundWordPair) (snd foundWordPair)
+        let sameLetters = filterSameLetters
+        print sameLetters
+
+
+
+findTheWord :: [String] -> (String, String)
+findTheWord [] = error "no word found"
+findTheWord [x] = error "no word found"
+findTheWord (x:xs)
+                  | isJust foundWord = (x, fromJust foundWord)
+                  | otherwise = findTheWord xs
+                  where foundWord = find (hasOneDiffChar x) xs
+
+hasOneDiffChar word1 word2 = countDiffChars (zip word1 word2) == 1
+
+countDiffChars zippedWords = sum ([1 | pair <- zippedWords, (fst pair) /= (snd pair)])
+
+filterSameLetters = [fst pair | pair <- zippedWords, (fst pair) == (snd pair)]
+
