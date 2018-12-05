d91cee4bd52fb7be9f7ece60e3ee19146d7babdb
Sjoerd <svanhouten@gmail.com>
2018-12-05 23:16:22 +0100

    day 5 pt 2 took 8m49.322s

--- src/day05alchemy/UnitDestroyer.hs
+++ src/day05alchemy/UnitDestroyer.hs
@@ -1,17 +1,38 @@
 import Data.Map
 import Data.Char
+import Data.List
 
+-- 8m49.322s
 main = do
   contents <- readFile "inputDay5.txt"
-  let idsWithUnits = reverse (ideify contents [])
-  let stableResult = causeChainReaction idsWithUnits (fromList idsWithUnits)
+  let stableResult = reactToStableResult contents
   print (Data.Map.size stableResult)
   print (elems stableResult)
+
+  print (findQuickestResult contents)
+
+findQuickestResult contents = do
+  let charToPolymerSize = getPolymerSizesForPolymersWithoutChar contents
+  head (sortOn (snd) charToPolymerSize)
+
+getPolymerSizesForPolymersWithoutChar :: String -> [(Char, Int)]
+getPolymerSizesForPolymersWithoutChar polymer = Prelude.map (\x -> reactWithoutChar x polymer) ['a'..'z']
+
+reactWithoutChar :: Char -> String -> (Char, Int)
+reactWithoutChar char polymer = do
+   let filteredPolymer = Data.List.filter (/= (toUpper char)) (Data.List.filter (/= char) polymer)
+   (char, Data.Map.size (reactToStableResult filteredPolymer))
+-----------------
+--REACTIONS
+-----------------
+reactToStableResult polymer = do
+  let idsWithUnits = reverse (ideify polymer [])
+  causeChainReaction idsWithUnits (fromList idsWithUnits)
 
 causeChainReaction :: [(Int, Char)] -> Map Int Char -> Map Int Char
 causeChainReaction [x] remainingUnits = remainingUnits
 causeChainReaction (x:y:[]) remainingUnits
-  | shouldReact (snd x) (snd y) = delete (fst x) (delete (fst y) remainingUnits)
+  | shouldReact (snd x) (snd y) = Data.Map.delete (fst x) (Data.Map.delete (fst y) remainingUnits)
   | otherwise = remainingUnits
 causeChainReaction (x:y:xs) remainingUnits
   | shouldReact (snd x) (snd y) = react (fst x) (fst y) remainingUnits
@@ -19,7 +40,7 @@ causeChainReaction (x:y:xs) remainingUnits
 
 react :: Int -> Int -> Map Int Char -> Map Int Char
 react x y units = do
-  let remainingUnits = (delete x (delete y units))
+  let remainingUnits = (Data.Map.delete x (Data.Map.delete y units))
   causeChainReaction (toList remainingUnits) remainingUnits
 
 ideify :: String -> [(Int, Char)] -> [(Int, Char)]
