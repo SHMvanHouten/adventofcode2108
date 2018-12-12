fb6681f19c64853b72a1c6ea0af7284208ab5f5d
Sjoerd <svanhouten@gmail.com>
2018-12-12 20:50:52 +0100

    day 12 intermediate commit to get all the values for the test

--- src/AOC/Day12/PlantSpreading.hs
+++ src/AOC/Day12/PlantSpreading.hs
@@ -1,2 +1,57 @@
 module AOC.Day12.PlantSpreading where
 
+import qualified Data.Map as Map
+
+passGenerations :: Integer -> Instructions -> ([Plant], Integer) -> [Plant]
+passGenerations amount instructions (plants, startingIndex)
+  | amount == 0 = plants
+  | otherwise = passGenerations (amount - 1) instructions (procreate plants instructions startingIndex [])
+
+-- to improve performance, we are adding each found new plant
+-- to the left of the list
+-- and reversing the result at the end (after we trim the empty pots at the end
+procreate :: [Plant] -> Instructions -> Integer -> [Plant] -> ([Plant], Integer)
+
+--case: end
+procreate (l1:l2:c:r1:[]) instructions startingIndex result = do
+  let rightEdge = getCenterValue (l1:l2:c:r1:'.':[]) instructions
+  let updatedResult = trimEmptyPotsAtEdge (rightEdge:result)
+  let reversedResult = trimEmptyPotsAtEdge $ reverse updatedResult
+  (reversedResult, startingIndex)
+
+--case: start
+procreate (l2:c:r1:r2:xs) instructions startingIndex []
+  | edgeChar == '#' = procreate (c:r1:r2:xs) instructions (startingIndex - 1) "#"
+  | otherwise = procreate (c:r1:r2:xs) instructions startingIndex "."
+  where edgeChar = getCenterValue ('.':l2:c:r1:r2:[]) instructions
+
+procreate (l1:l2:c:r1:r2:xs) instructions startingIndex result = do
+  let updatedResult = (getCenterValue (l1:l2:c:r1:r2:[]) instructions):result
+  procreate (l2:c:r1:r2:xs) instructions startingIndex updatedResult
+
+trimEmptyPotsAtEdge :: [Plant] -> [Plant]
+trimEmptyPotsAtEdge (x:xs)
+  | x == '#' = x:xs
+  |otherwise = trimEmptyPotsAtEdge xs
+
+-- calculate left edge and startingindex-- if found plant
+
+getCenterValue :: [Plant] -> Instructions -> Char
+getCenterValue fivePots instructions= instructions Map.! fivePots
+
+parseRawPlantInstructions :: String -> (Instructions, [Plant])
+parseRawPlantInstructions rawString = (Map.fromList $ map toInstruction (tail $ tail lineSplit), last $ words $head lineSplit)
+  where lineSplit = lines rawString
+
+toInstruction :: String -> Instruction
+toInstruction rawInstruction = (head split, head $ last split)
+  where split = words rawInstruction
+
+type Plant = Char
+
+type Instructions = Map.Map String Char
+type Instruction = (String, Char)
+--data Plant = Plant {
+--  id :: Int,
+--  isPlant :: Bool
+--} deriving (Show, Eq)
--- test/AOC/Day12/PlantSpreadingSpec.hs
+++ test/AOC/Day12/PlantSpreadingSpec.hs
@@ -1,2 +1,94 @@
 module AOC.Day12.PlantSpreadingSpec where
 
+import Test.Hspec
+import Test.QuickCheck
+import qualified Data.Map as Map
+import AOC.Day12.PlantSpreading
+
+
+main :: IO ()
+main = hspec spec
+
+spec :: Spec
+spec = do
+
+  describe "find the value after 20 generations of the plants " $ do
+    it "runs through 20 generations of plants and gets the value of it" $ do
+      pending
+
+  describe "find the next generation of the plants " $ do
+    it "gets the next generation" $ do
+        pending
+
+  describe "parse" $ do
+    it "turns the input into a pair of the plantrow and a Map of instructions" $ do
+      let input = "initial state: #.#\n" ++
+                  "\n"++
+                  "..#.. => #\n" ++
+                  "..### => .\n"
+      parseRawPlantInstructions input `shouldBe` ((Map.fromList [("..#..", '#'), ("..###", '.')]), "#.#")
+
+  describe "passGenerations" $ do
+    it "should calculate what the state of the plants is after one generation has passed" $ do
+      let input = parseRawPlantInstructions testInput
+      passGenerations 1 (fst input) ((snd input), 0) `shouldBe` "#...#....#.....#..#..#..#"
+
+  describe "procreate end" $ do
+    it "end case: should trim the edges and reverse" $ do
+      let instructions = fst $ parseRawPlantInstructions testInput
+      procreate "#..."  instructions 0 ".##########.."`shouldBe` ("##########", 0)
+
+  describe "getCenterValue" $ do
+    it "gives the result of the 5 pots procreating" $ do
+      let instructions = fst $ parseRawPlantInstructions testInput
+      getCenterValue "...##" instructions `shouldBe` '#'
+
+  describe "trimPotsAtEgde" $ do
+    it "trims the left edge of empty pots" $ do
+      trimEmptyPotsAtEdge ".....######" `shouldBe` "######"
+
+testInput = "initial state: #..#.#..##......###...###\n"++
+            "\n"++
+            "..... => .\n"++
+            "....# => .\n"++
+            "...#. => .\n"++
+            "...## => #\n"++
+            "..#.. => #\n"++
+            "..#.# => .\n"++
+            "..### => .\n"++
+            ".#... => #\n"++
+            ".#..# => .\n"++
+            ".#.#. => #\n"++
+            ".#.## => #\n"++
+            ".##.. => #\n"++
+            ".##.# => .\n"++
+            ".###. => .\n"++
+            ".#### => #\n"++
+            "#.... => .\n"++
+            "#.#.. => .\n"++
+            "#.#.# => #\n"++
+            "#.##. => .\n"++
+            "#.### => #\n"++
+            "##... => .\n"++
+            "##.#. => #\n"++
+            "##.## => #\n"++
+            "###.. => #\n"++
+            "###.# => #\n"++
+            "####. => #\n"++
+            "##### => .\n"
+
+
+-- ...## => #
+-- ..#.. => #
+-- .#... => #
+-- .#.#. => #
+-- .#.## => #
+-- .##.. => #
+-- .#### => #
+-- #.#.# => #
+-- #.### => #
+-- ##.#. => #
+-- ##.## => #
+-- ###.. => #
+-- ###.# => #
+-- ####. => #
